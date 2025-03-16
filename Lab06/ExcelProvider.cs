6e2c229243e157880b8d3c48f4d614b46700129c
Le Quoc Cuong <lequoccuong06032004@gmail.com>
2025-03-16 18:47:32 +0700

    Add project files.

---
+++ Lab06/ExcelProvider.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.IO;
+using System.Data;
+using OfficeOpenXml;
+using ExcelDataReader;
+
+namespace Test_QuocCuong
+{
+    internal class ExcelProvider
+    {
+        private static DataTable _excelDataTable;
+
+        public static DataTable ReadExcel(string filePath)
+        {
+            if (_excelDataTable != null)
+                return _excelDataTable;
+
+            System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+            using (var stream = File.Open(filePath, FileMode.Open, FileAccess.Read))
+            {
+                using (var reader = ExcelReaderFactory.CreateReader(stream))
+                {
+                    DataSet dataSet = reader.AsDataSet();
+                    _excelDataTable = dataSet.Tables[0];
+                    return _excelDataTable;
+                }
+            }
+        }
+
+        public static void WriteResultToExcel(string filePath, string sheetName, int rowIndex, string actualResult, string status)
+        {
+            try
+            {
+                ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+                using (ExcelPackage package = new ExcelPackage(new FileInfo(filePath)))
+                {
+                    ExcelWorksheet worksheet = package.Workbook.Worksheets[sheetName] ?? package.Workbook.Worksheets[0];
+
+                    int colActualResult = 7;
+                    int colStatus = 8;
+
+                    int totalRows = worksheet.Dimension?.Rows ?? 0;
+
+                    worksheet.Cells[rowIndex, colActualResult].Value = actualResult;
+                    worksheet.Cells[rowIndex, colStatus].Value = status;
+
+                    package.Save();
+                    Console.WriteLine($"excel bri");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Lỗi khi ghi vào Excel: " + ex.Message);
+            }
+        }
+
+        public static bool ValidateExcelData(string filePath, string sheetName, int expectedTotal, int expectedCurrentMonthRevenue)
+        {
+            try
+            {
+                DataTable excelData = ReadExcel(filePath);
+                if (excelData == null) return false;
+
+                int sum = 0;
+                int currentMonth = DateTime.Now.Month;
+                int currentMonthRevenue = 0;
+
+                for (int i = 0; i < 12; i++)
+                {
+                    if (excelData.Rows.Count > i + 1 && excelData.Columns.Count > 1)
+                    {
+                        object cellValue = excelData.Rows[i + 1][1];
+                        if (cellValue != null && int.TryParse(cellValue.ToString(), out int value))
+                        {
+                            sum += value;
+                            if (i + 1 == currentMonth) currentMonthRevenue = value;
+                        }
+                    }
+                }
+                return sum == expectedTotal && currentMonthRevenue == expectedCurrentMonthRevenue;
+            }
+            catch(Exception ex) {
+            
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+        }
+
+
+
+        public static bool ValidateCustomerData(string filePath, string sheetName, int totalRow, string firstCus, string lastCus)
+        {
+            try
+            {
+                DataTable excelData = ReadExcel(filePath);
+                if (excelData == null)
+                {
+                    return false;
+                }
+                int dataRowCount = excelData.Rows.Count-1;
+                if (dataRowCount != totalRow)
+                {
+                    return false;
+                }
+
+                if (dataRowCount > 0)
+                {
+                    string firstCustomer = excelData.Rows[1][0].ToString().Trim();
+                    if (!firstCustomer.Equals(firstCus, StringComparison.OrdinalIgnoreCase))
+                    {
+                        return false;
+                    }
+
+                    string lastCustomer = excelData.Rows[dataRowCount - 1][0].ToString().Trim();
+                    if (!lastCustomer.Equals(lastCus, StringComparison.OrdinalIgnoreCase))
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+
+        public static bool ValidateEmptyCustomerData(string filePath, string sheetName, string[] expectedCot)
+        {
+            try
+            {
+                DataTable excelData = ReadExcel(filePath);
+
+                if (excelData == null || excelData.Columns.Count < expectedCot.Length)
+                {
+                    return false;
+                }
+
+                for (int i = 0; i < expectedCot.Length; i++)
+                {
+                    string actualHeader = excelData.Columns[i].ColumnName.Trim();
+                    if (!actualHeader.Equals(expectedCot[i], StringComparison.OrdinalIgnoreCase))
+                    {
+                        return false;
+                    }
+                }
+                if (excelData.Rows.Count > 0)
+                {
+                    return false;
+                }
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Lỗi khi đọc file Excel: " + ex.Message);
+                return false;
+            }
+        }
+    }
+}
---
+++ Lab06/UnitTest1.cs
@@ -0,0 +1,149 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
+using System;
+using System.Threading;
+using Test_QuocCuong;
+using static Microsoft.IO.RecyclableMemoryStreamManager;
+
+namespace AutomationExerciseTests
+{
+    public class ProductTests
+    {
+        private IWebDriver driver;
+        string root = AppDomain.CurrentDomain.BaseDirectory;
+        string[] expectedHeaders = { "Họ Tên", "Tên Khách Sạn", "Email", "Số Điện Thoại", "Ngày Sinh", "Giới Tính", "Phương Thức Thanh Toán", "Ngày Check-in", "Ngày Check-out", "Tổng Giá", "Tổng Số Phòng", "Trạng Thái Hóa Đơn" };
+        [SetUp]
+        public void Setup()
+        {
+            ChromeOptions options = new ChromeOptions();
+            options.AddUserProfilePreference("download.default_directory", root);
+            options.AddUserProfilePreference("download.prompt_for_download", false);
+            options.AddUserProfilePreference("download.directory_upgrade", true);
+            options.AddUserProfilePreference("safebrowsing.enabled", true);
+            options.AddUserProfilePreference("credentials_enable_service", false);
+            options.AddUserProfilePreference("profile.password_manager_enabled", false);
+            string filePath = Path.Combine(root, "HoaDon.xlsx");
+            string filePathh = Path.Combine(root, "BaoCaoDoanhThu.xlsx");
+            driver = new ChromeDriver(options);
+            if (File.Exists(filePath))
+            {
+                File.Delete(filePath);
+            }
+            if (File.Exists(filePathh))
+            {
+                File.Delete(filePathh);
+            }
+            driver.Navigate().GoToUrl("http://localhost:3000/loginOwner");
+            IWebElement email = driver.FindElement(By.XPath("/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[2]/div[1]/div[3]/form[1]/div[1]/div[1]/div[2]/div[1]/div[1]/span[1]/input[1]"));
+            email.SendKeys("qcuong@gmail.com");
+            IWebElement pass = driver.FindElement(By.XPath("/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[2]/div[1]/div[3]/form[1]/div[2]/div[1]/div[2]/div[1]/div[1]/span[1]/input[1]"));
+            pass.SendKeys("123456");
+            IWebElement btnLogin = driver.FindElement(By.XPath("/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[2]/div[1]/div[3]/form[1]/div[3]/div[1]/div[1]/div[1]/div[1]/button[1]"));
+            btnLogin.Click();
+            Thread.Sleep(2000);
+        }
+
+        //[Test]
+
+        //public void ActiveCustomer()
+        //{
+        //    driver.Navigate().GoToUrl("http://localhost:3000/loginOwner");
+        //    IWebElement email = driver.FindElement(By.XPath("/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[2]/div[1]/div[3]/form[1]/div[1]/div[1]/div[2]/div[1]/div[1]/span[1]/input[1]"));
+        //    email.SendKeys("qcuong@gmail.com");
+        //    IWebElement pass = driver.FindElement(By.XPath("/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[2]/div[1]/div[3]/form[1]/div[2]/div[1]/div[2]/div[1]/div[1]/span[1]/input[1]"));
+        //    pass.SendKeys("123456");
+        //    IWebElement btnLogin = driver.FindElement(By.XPath("/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[2]/div[1]/div[3]/form[1]/div[3]/div[1]/div[1]/div[1]/div[1]/button[1]"));
+        //    btnLogin.Click();
+        //    Thread.Sleep(2000);
+        //    IWebElement cusTab = driver.FindElement(By.XPath("/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/div[1]/a[3]/div[1]/*[name()='svg'][1]"));
+        //    cusTab.Click();
+        //    Thread.Sleep(2000);
+        //    IWebElement deActivate = driver.FindElement(By.XPath("/html[1]/body[1]/div[1]/div[1]/div[1]/div[2]/div[2]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/table[1]/tbody[1]/tr[1]/td[4]/span[1]"));
+        //    deActivate.Click();
+        //    IWebElement txtReason = driver.FindElement(By.XPath("/html[1]/body[1]/div[2]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/input[1]"));
+        //    txtReason.SendKeys("kho noi");
+        //    IWebElement btnAccept = driver.FindElement(By.XPath("/html[1]/body[1]/div[2]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/button[2]"));
+        //    btnAccept.Click();
+        //    bool a = deActivate.Text == "Vô hiệu hóa";
+        //    string b = a.ToString();
+        //    //ExcelProvider.WriteResultToExcel("C:\\Users\\thanh\\source\\repos\\Lab06\\Lab06\\bin\\Debug\\net8.0\\TestCase_BDCLPM_HK2.xlsx", "TestCase_QuocCuong",7, "kho noi", "gay");
+        //    Assert.IsTrue(true);
+        //}
+
+        [Test]
+
+        public void EX04_TestCase()
+        {
+            IWebElement mainTab = driver.FindElement(By.XPath("/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/div[1]/a[1]/div[1]"));
+            mainTab.Click();
+            Thread.Sleep(2000);
+            IWebElement btnExport = driver.FindElement(By.XPath("/html[1]/body[1]/div[1]/div[1]/div[1]/div[2]/div[2]/div[1]/button[1]"));
+            btnExport.Click();
+            IWebElement txtMonth = driver.FindElement(By.XPath("/html[1]/body[1]/div[1]/div[1]/div[1]/div[2]/div[2]/div[2]/div[1]/div[1]/h1[1]"));
+            string txtThangStr = txtMonth.Text;
+            int txtThang = int.Parse(new string(txtThangStr.Where(char.IsDigit).ToArray()));
+            IWebElement txtYear = driver.FindElement(By.XPath("/html[1]/body[1]/div[1]/div[1]/div[1]/div[2]/div[2]/div[2]/div[2]/div[1]/h1[1]"));
+            string txtYearStr = txtMonth.Text;
+
+            int txtYearr = int.Parse(new string(txtYearStr.Where(char.IsDigit).ToArray()));
+    
+            Thread.Sleep(2000);
+            string patha = Path.Combine(root, "BaoCaoDoanhThu.xlsx");
+            bool isValid = ExcelProvider.ValidateExcelData(patha, "data", txtYearr, txtThang);
+            string actual = isValid ? "File Excel hiển thị đúng doanh thu trong vòng 2 năm" : "File Excel hiển thị sai";
+            string result = isValid ? "Passed" : "Failed";
+            string path = Path.Combine(root, "TestCase_BDCLPM_HK2.xlsx");
+            ExcelProvider.WriteResultToExcel(path, "TestCase_QuocCuong", 36, actual, result);
+            Assert.IsTrue(true);
+        }
+        [Test]
+
+        public void EX01_EX02_TestCase()
+        {
+            IWebElement invoiceTab = driver.FindElement(By.XPath("/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/div[1]/a[6]"));
+            invoiceTab.Click();
+            Thread.Sleep(2000);
+            IWebElement btnExport = driver.FindElement(By.XPath("/html[1]/body[1]/div[1]/div[1]/div[1]/div[2]/div[2]/div[1]/div[2]/div[1]/button[1]"));
+            btnExport.Click();
+            IWebElement firstCus = driver.FindElement(By.XPath("/html[1]/body[1]/div[1]/div[1]/div[1]/div[2]/div[2]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/table[1]/tbody[1]/tr[2]/td[1]/div[1]"));
+           Console.WriteLine(firstCus.Text);
+            int totalRows = driver.FindElements(By.XPath("//table/tbody/tr")).Count;
+            IWebElement lastCus = driver.FindElement(By.XPath($"//table/tbody/tr[{totalRows}]/td[1]"));
+            Console.WriteLine(lastCus.Text);
+            string path = Path.Combine(root, "HoaDon.xlsx");
+            Thread.Sleep(3000);
+            bool isValid = ExcelProvider.ValidateCustomerData(path, "data", totalRows-1, firstCus.Text, lastCus.Text);
+            string actual = isValid ? "File excel bên trong bao gồm 1 dòng các thông tin của hóa đơn" : "File Excel hiển thị sai";
+            string result = isValid ? "Passed" : "Failed";
+            ExcelProvider.WriteResultToExcel("TestCase_BDCLPM_HK2.xlsx", "TestCase_QuocCuong",27, actual, result);
+            ExcelProvider.WriteResultToExcel("TestCase_BDCLPM_HK2.xlsx", "TestCase_QuocCuong", 30, actual, result);
+            Assert.IsTrue(true);
+        }
+        [Test]
+
+        public void EX03_TestCase()
+        {
+            IWebElement invoiceTab = driver.FindElement(By.XPath("/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/div[1]/a[6]"));
+            invoiceTab.Click();
+            Thread.Sleep(2000);
+            IWebElement btnExport = driver.FindElement(By.XPath("/html[1]/body[1]/div[1]/div[1]/div[1]/div[2]/div[2]/div[1]/div[2]/div[1]/button[1]"));
+            btnExport.Click();
+            string path = Path.Combine(root, "HoaDon.xlsx");
+            Thread.Sleep(3000);
+            bool isValid = ExcelProvider.ValidateEmptyCustomerData(path, "data", expectedHeaders);
+            string actual = isValid ? "File excel không có thông tin của 1 dòng hóa đơn bất kỳ" : "File Excel hiển thị sai";
+            string result = isValid ? "Passed" : "Failed";
+            ExcelProvider.WriteResultToExcel("TestCase_BDCLPM_HK2.xlsx", "TestCase_QuocCuong", 33, actual, result);
+            Assert.IsTrue(true);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Thread.Sleep(3000);
+            driver.Quit();
+        }
+    }
+}
